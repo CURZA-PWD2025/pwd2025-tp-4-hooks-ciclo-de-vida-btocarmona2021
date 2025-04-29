04dfcc39155931478d91d1adbdf13f4a674c2333
Alberto Carmona <btocarmona@gmail.com>
2025-04-28 21:01:50 -0300

    Listado implementado

---
+++ tp4/src/components/ListaComponent.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { ref ,onMounted } from 'vue';
+import { profesiones } from "../data/profesiones";
+import type { Profesion } from '@/interface/profesion';
+const listaProfesiones = ref<Profesion[]>([]);
+
+
+
+
+onMounted(() => {
+  setTimeout(() => {
+    listaProfesiones.value = profesiones;
+  }, 3000);
+});
+</script>
+
+<template>
+  <div></div>
+</template>
+
+<style scoped></style>
